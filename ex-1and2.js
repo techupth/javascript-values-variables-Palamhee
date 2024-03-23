dcb5d1c76f9d710eb48aaddf6f387e817e9ead40
Palamhee <palarat.pj@gmail.com>
2024-03-23 20:10:52 +1300

    hand in js values and variables exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+let netPrice = 15000;
+let francaiseNumber = 1500;
+console.log(name, surName, address, restaurantName, netPrice, francaiseNumber);
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(C0), 81641";
+netPrice = 5000;
+francaiseNumber = 2500;
+console.log(address, netPrice, francaiseNumber);
